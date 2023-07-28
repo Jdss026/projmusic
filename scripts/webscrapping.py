ec32c1e2a6b8aff868d1dea7466dd9c816c67ae2
Jdss026 <jdss026@gmail.com>
2023-07-28 14:32:29 -0300

    fix search for positive dataset

--- scripts/webscrapping.py
+++ scripts/webscrapping.py
@@ -1,29 +1,36 @@
 from lyricsgenius import Genius
 import json
-import codecs
 
 # cria acesso a API
 
 token = 'SRY7bkiOpAIaIm6Uh_fz0xyv3cEpKzph9RTpe6bQ4xVRfnwPdEeoCZ1oTeesYR7a'
 
 genius = Genius(token, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
+genius.verbose = True
+genius.remove_section_headers = True
+genius.skip_non_songs = True
 
 # busca artista 
-artist = genius.search_artist("Gilberto Gil", max_songs=10,sort="title")
+artist = genius.search_artist("Gilberto Gil", max_songs=500, sort="title")
+
+artist.save_lyrics(overwrite=True, filename='lyrics.json')
+
+with open('lyrics.json', 'r') as file:
+    data = json.load(file)
+
 
 # busca musicas do autor e salva em dicionario 
-dict_songs_pos = dict()
-for song in artist.songs:
-    title = song.title.encode('utf-8')
-    title = codecs.decode(title, "UTF-8")
-    lyrics = song.lyrics.encode('utf-8')
-    lyrics = codecs.decode(lyrics, "UTF-8")
-    dict_songs_pos[str(title)] = str(lyrics)
+dict_songs_pos = {}
+tam_songs = len(artist.songs)
 
+for song in range(tam_songs):
+    lyrics = data['songs'][song]['lyrics'] #.split("Lyrics")[song].split("You might also likeEmbed")[0]
+    title = data['songs'][song]['title']
+    print(type(title))
+    dict_songs_pos[title] = lyrics
 
-# salva musicas em json no schema:{"song": "lyrics", ... }
+# salva em json
 file = './data/data_music.json'
-json_data = json.dumps(dict_songs_pos)
 
 with open(file, "w") as json_file:
-    json.dump(json_data, json_file, indent=4)
+    json.dump(dict_songs_pos, json_file, indent=4)
